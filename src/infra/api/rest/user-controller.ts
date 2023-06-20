10da491160503478f71e13be74251597192d2080
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-06-20 16:01:06 -0300

    add route find user

--- src/infra/api/rest/user-controller.ts
+++ src/infra/api/rest/user-controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
 import { CreateUserUseCase } from '../../../domain/usecases/create-user-usecase';
+import { FindUserUseCase } from '../../../domain/usecases/find-user-usecase';
 
 export class UserController {
   constructor(
     private readonly createUserUseCase: CreateUserUseCase,
+    private readonly findUserUseCase: FindUserUseCase,
   ) { }
 
   async createUser(request: Request, response: Response) {
@@ -18,6 +20,22 @@ export class UserController {
       return response.status(400).json({ message: error.message });
     }
   }
+
+
+  async findUser(request: Request, response: Response) {
+
+    try {
+      const email = request.query.email as string;
+
+      const user = await this.findUserUseCase.execute(email);
+
+      return response.json(user);
+    } catch (error: any) {
+      return response.status(400).json({ message: error.message });
+    }
+  }
+
+
 
 
 }
--- src/infra/api/rest/user-factory.ts
+++ src/infra/api/rest/user-factory.ts
@@ -1,4 +1,5 @@
 import { CreateUserUseCase } from '../../../domain/usecases/create-user-usecase';
+import { FindUserUseCase } from '../../../domain/usecases/find-user-usecase';
 import { Encrypter } from '../../helper/encrypter';
 import { UserRepository } from '../repositories/prisma/user-repository';
 import { UserController } from './user-controller';
@@ -7,6 +8,8 @@ export const userRepository = new UserRepository();
 const encrypter = new Encrypter();
 
 const createUserUseCase = new CreateUserUseCase(userRepository, encrypter);
-const userController = new UserController(createUserUseCase);
+const findUserUseCase = new FindUserUseCase(userRepository);
+
+const userController = new UserController(createUserUseCase, findUserUseCase,);
 
 export { userController };
