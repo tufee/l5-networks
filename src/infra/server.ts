fe7bc05be94d54948429888be59b5345db05d486
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-06-20 15:51:27 -0300

    add server

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,6 +1,8 @@
+import 'reflect-metadata';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
 import express from 'express';
+import { userRouter } from './api/rest/user-routes';
 import { logger } from './helper/logger';
 dotenv.config();
 
@@ -9,6 +11,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(userRouter);
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => logger.info(`Server started on port ${port}`));
