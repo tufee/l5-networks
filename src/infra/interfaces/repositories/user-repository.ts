8b301274834e83d22218977124addf07adaf838c
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-06-20 16:21:30 -0300

    add upload and download interfaces

--- src/infra/interfaces/repositories/user-repository.ts
+++ src/infra/interfaces/repositories/user-repository.ts
@@ -1,7 +1,9 @@
-import { ICreateUserData, IUserResponse } from '../../../domain/usecases/create-user-dto';
+import { ICreateUserData, IUserResponse, IUserUpload } from '../../../domain/usecases/create-user-dto';
 
 export interface IUserRepository {
   save(user: ICreateUserData): Promise<IUserResponse>;
   findByEmail(email: string): Promise<IUserResponse | null>;
   delete(email: string): Promise<void>;
+  upload(data: IUserUpload): Promise<void>;
+  download(email: string): Promise<any>;
 }
