680a681320d79773b905ae4e348e355a64345652
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-06-20 15:50:20 -0300

    add encrypter and tests

---
+++ src/infra/helper/encrypter.ts
@@ -0,0 +1,16 @@
+import bcrypt from 'bcrypt';
+import { IEncrypter } from '../interfaces/cryptography/encrypter';
+
+export class Encrypter implements IEncrypter {
+
+  async encrypt(password: string): Promise<string> {
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+
+    return await bcrypt.hash(password, salt);
+  }
+
+  async decrypt(password: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(password, hash);
+  }
+}
