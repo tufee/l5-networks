61e2933ed0b48b1b9d17886418247cb609e573a5
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-06-20 15:52:28 -0300

    add user repository

---
+++ src/infra/api/repositories/prisma/user-repository.ts
@@ -0,0 +1,33 @@
+import { ICreateUserData, IUserResponse } from '../../../../domain/usecases/create-user-dto';
+import { IUserRepository } from '../../../interfaces/repositories/user-repository';
+import { prisma } from './prisma-client';
+
+export class UserRepository implements IUserRepository {
+
+  async findByEmail(email: string): Promise<IUserResponse | null> {
+    return await prisma.user.findUnique({
+      where: {
+        email
+      },
+      select: {
+        id: true,
+        name: true,
+        login: true,
+        email: true,
+      }
+    });
+  }
+
+  async save(user: ICreateUserData): Promise<IUserResponse> {
+    return await prisma.user.create({
+      data: {
+        name: user.name,
+        login: user.login,
+        email: user.email,
+        password: user.password
+      }
+    });
+  }
+
+
+}
