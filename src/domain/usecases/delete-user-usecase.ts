0347bc5ba431c1bd39813b98be9b68421496a5dc
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2023-07-01 03:30:16 -0300

    add lint

--- src/domain/usecases/delete-user-usecase.ts
+++ src/domain/usecases/delete-user-usecase.ts
@@ -1,10 +1,10 @@
-import { UserRepository } from '../../infra/api/repositories/prisma/user-repository';
+import {UserRepository} from '../../infra/api/repositories/prisma/user-repository';
 import * as validator from '../../infra/helper/validator';
 
 export class DeleteUserUseCase {
   constructor(
     private readonly userPostgresRepository: UserRepository,
-  ) { }
+  ) {}
 
   async execute(email: string): Promise<void> {
 
