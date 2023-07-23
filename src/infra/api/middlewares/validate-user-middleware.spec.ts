af47709fad74e93002fafd9b451c3e0ebd85b8cb
tufe <pauloalmeida.jrr@gmail.com>
2023-07-23 20:28:11 -0300

    refactor do test da classe validate user middleware

--- src/infra/api/middlewares/validate-user-middleware.spec.ts
+++ src/infra/api/middlewares/validate-user-middleware.spec.ts
@@ -1,50 +1,127 @@
-import request from 'supertest';
-import { beforeEach, describe, expect, it } from 'vitest';
-import { app } from '../../server';
-import { prisma } from '../repositories/prisma/prisma-client';
+import { NextFunction, Request, Response } from 'express';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import * as validator from '../../helper/validator';
+import {
+  validateCreateUserData,
+  validateUser
+} from './validate-user-middleware';
 
-describe('ValidateUserMiddleware', async () => {
+describe('validateUserMiddleware', () => {
+  let mockRequest: Partial<Request>;
+  let mockResponse: Partial<Response>;
+  let mockNextFunction: NextFunction;
 
-  beforeEach(async () => {
-    await prisma.user.deleteMany();
-    await prisma.upload.deleteMany();
-  });
-
-  it('should return 200 code if have a valid payload', async () => {
-
-    const data = {
-      name: 'John Doe',
-      login: 'john',
-      email: 'john@mail.com',
-      emailConfirmation: 'john@mail.com',
-      password: '12345678',
-      passwordConfirmation: '12345678'
+  beforeEach(() => {
+    mockRequest = {
+      body: {
+        name: 'John Doe',
+        login: 'johndoe',
+        email: 'johndoe@example.com',
+        emailConfirmation: 'johndoe@example.com',
+        password: 'password123',
+        passwordConfirmation: 'password123',
+      },
+      query: {
+        user: 'john.doe@example.com',
+      }
     };
-
-    const response = await request(app)
-      .post('/createUser')
-      .send(data);
-
-    expect(response.status).toBe(200);
-  });
-
-  it('should return 400 error if any field fails validation', async () => {
-    const data = {
-      name: '',
-      login: '',
-      email: 'john@@@mail.com',
-      emailConfirmation: 'john@mail.com',
-      password: '1234',
-      passwordConfirmation: '1234'
+    mockResponse = {
+      status: vi.fn().mockReturnThis(),
+      json: vi.fn(),
     };
-
-    const response = await request(app)
-      .post('/createUser')
-      .send(data);
-
-    expect(response.status).toBe(400);
-    expect(response.body.error).toBeTruthy();
-
+    mockNextFunction = vi.fn() as any;
   });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  describe('validateCreateUserData', () => {
+    it('should call the next function if all validations pass', () => {
+      validateCreateUserData(
+        mockRequest as Request, mockResponse as Response, mockNextFunction
+      );
+      expect(mockNextFunction).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call validator functions with the correct arguments', () => {
+      const validateNameMock = vi.spyOn(validator, 'validateName');
+      const validateLoginMock = vi.spyOn(validator, 'validateLogin');
+      const validateEmailMock = vi.spyOn(validator, 'validateEmail');
+      const validateEmailConfirmationMock = vi.spyOn(
+        validator, 'validateEmailConfirmation'
+      );
+      const validatePasswordMock = vi.spyOn(validator, 'validatePassword');
+      const validatePasswordConfirmationMock = vi.spyOn(
+        validator, 'validatePasswordConfirmation'
+      );
+
+      validateCreateUserData(
+        mockRequest as Request, mockResponse as Response, mockNextFunction
+      );
+
+      expect(validateNameMock).toHaveBeenCalledWith(mockRequest.body.name);
+      expect(validateLoginMock).toHaveBeenCalledWith(mockRequest.body.login);
+      expect(validateEmailMock).toHaveBeenCalledWith(mockRequest.body.email);
+      expect(validateEmailConfirmationMock).toHaveBeenCalledWith(
+        mockRequest.body.email,
+        mockRequest.body.emailConfirmation
+      );
+      expect(validatePasswordMock).toHaveBeenCalledWith(
+        mockRequest.body.password);
+      expect(validatePasswordConfirmationMock).toHaveBeenCalledWith(
+        mockRequest.body.password,
+        mockRequest.body.passwordConfirmation
+      );
+    });
+
+    it('should return a 400 status code and error message if a validation fails',
+      () => {
+        const errorMessage = 'Name is required';
+        vi.spyOn(validator, 'validateName').mockImplementation(() => {
+          throw new Error(errorMessage);
+        });
+
+        validateCreateUserData(
+          mockRequest as Request, mockResponse as Response, mockNextFunction
+        );
+
+        expect(mockResponse.status).toHaveBeenCalledWith(400);
+        expect(mockResponse.json).toHaveBeenCalledWith({ error: errorMessage });
+      });
+  });
+
+  describe('validateUser', () => {
+    it('should call the next function if validation passes', () => {
+      validateUser(
+        mockRequest as Request, mockResponse as Response, mockNextFunction);
+      expect(mockNextFunction).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call the validator function with the correct argument', () => {
+      const validateUserMock = vi.spyOn(validator, 'validateUser');
+
+      validateUser(
+        mockRequest as Request, mockResponse as Response, mockNextFunction
+      );
+
+      expect(validateUserMock).toHaveBeenCalledWith(mockRequest.query?.user);
+    });
+
+    it('should return a 400 status code and error message if the validation fails',
+      () => {
+        const errorMessage = 'User is required';
+        vi.spyOn(validator, 'validateUser').mockImplementation(() => {
+          throw new Error(errorMessage);
+        });
+
+        validateUser(
+          mockRequest as Request, mockResponse as Response, mockNextFunction
+        );
+
+        expect(mockResponse.status).toHaveBeenCalledWith(400);
+        expect(mockResponse.json).toHaveBeenCalledWith({ error: errorMessage });
+      });
+  });
+
 });
-
